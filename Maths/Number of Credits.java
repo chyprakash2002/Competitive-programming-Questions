2f46e19f416910fac52db4859879f0477a735278
Prakash Chaudhary <chyprakash2002@gmail.com>
2022-12-08 11:25:56 +0530

    Add files via upload

---
+++ Maths/Number of Credits.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t-- !=0){
+		    int X = sc.nextInt();
+		    int Y = sc.nextInt();
+		    int Z = sc.nextInt();
+		    int total = X*4 + Y*2 + Z*0;
+		    System.out.println(total);
+	}
+}
+}
